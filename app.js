309d317d2918d26d4b449df372484da46383df01
carlos <cgcarlosg@hotmail.com>
2021-02-22 18:52:34 -0500

    include local storage and add functions to addbook to library and draw library

--- app.js
+++ app.js
@@ -15,3 +15,44 @@ class Book {
     }
 }
 
+class Library {
+    constructor() {
+        this._library = (localStorage.library == undefined) ? [] : JSON.parse(localStorage.library);
+    }
+
+    addBookToLibrary(title, author, read) {
+        this._library.push(new Book(title, author, read));
+    }
+
+    drawLibrary() {
+        const table = document.querySelector("tbody");
+        table.innerHTML = "";
+        this._library.forEach(book => {
+            let read = (book.read) ? '✓' : '✕';
+            table.innerHTML += `<tr>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td><a href="#" class="read-status" data-value="${this._library.indexOf(book)}">${read}</span></td>
+            <td><button data-value="${this._library.indexOf(book)}">Delete</button></td>
+            </tr>`
+        });
+    
+        table.querySelectorAll("button").forEach(button => {
+            button.addEventListener('click', () => {
+                let index = this['data-value'];
+                this._library.splice(index, 1);
+                this.drawLibrary();
+            })
+        })
+    
+        table.querySelectorAll('.read-status').forEach(a => {
+            a.addEventListener('click', (event) => {
+                let index = event.target.getAttribute("data-value");
+                this._library[index].read = (this._library[index].read) ? false : true;
+                this.drawLibrary();
+            })
+        })
+    
+        localStorage.library = JSON.stringify(this._library);
+    }
+}
