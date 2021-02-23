41f9be90380d2e30c7cf0a5e618abc51e4dd5d0f
carlos <cgcarlosg@hotmail.com>
2021-02-22 23:05:04 -0500

    solve issues in methods

--- app.js
+++ app.js
@@ -1,56 +1,69 @@
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const table = document.getElementById("tabla");
-const boton = document.getElementById("boton");
+const table = document.getElementById("table");
+const button = document.getElementById("button");
 
-function agregarTarea() {
+class Book {
+    constructor(title, author, pages) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+    }
+  }
+
+
+function addBook() {
   const list = document.getElementById('book-list');
   const row = document.createElement("tr");
 
   row.innerHTML = `
-                    <td> <input type="checkbox" onClick="completar()" /> </td>
+                    <td> <input type="checkbox" onClick="update()" /> </td>
                     <td style="flex-grow: 2"> ${title.value} </td>
                     <td style="flex-grow: 2"> ${author.value} </td>
                     <td style="flex-grow: 2"> ${pages.value} </td>
-                    <td> <span onClick="borrar()"> X </span> </td>
+                    <td> <span onClick="remove()"> X </span> </td>
                     `;
 
   list.appendChild(row);
 }
 
-boton.addEventListener("click", function (e) {
+button.addEventListener("click", function (e) {
   if (title.value === "" || author.value === "" || pages.value === "" ) {
-    validacion("introduce una tarea", "fallo");
+    validation("Do not leave empty fields", "unsuccess");
   } else {
-    agregarTarea();
-    validacion("Tarea añadida con éxito", "exito");
+    addBook();
+    validation("Booked Added", "success");
   }
 });
 
-function borrar(event) {
+function remove(event) {
   this.event.target.parentElement.parentElement.remove();
 }
 
-function validacion(mensaje, clase) {
+function validation(message, className) {
   const div = document.createElement("div");
-  div.className = clase;
-  div.appendChild(document.createTextNode(mensaje));
+  div.className = className;
+  div.appendChild(document.createTextNode(message));
 
   const titulo = document.querySelector("h1");
 
   titulo.insertBefore(div, null);
 
   setTimeout(function () {
-    document.querySelector(`.${clase}`).remove();
+    document.querySelector(`.${className}`).remove();
   }, 3000);
 }
 
-function completar(event) {
+function update(event) {
   if (this.event.target.checked) {
-    this.event.target.parentElement.parentElement.classList.add("completado");
+    this.event.target.parentElement.parentElement.classList.add("read");
   } else {
-    this.event.target.parentElement.parentElement.classList.remove("completado");
+    this.event.target.parentElement.parentElement.classList.remove("read");
   }
 }
+
+function clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+  }
+
   
