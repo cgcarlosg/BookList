8a68403dae2ee0ba2162bab4a4fe49d8ecaac5cb
carlos <cgcarlosg@hotmail.com>
2021-02-23 13:09:06 -0500

    fixed linters

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-const table = document.getElementById("table");
-const button = document.getElementById("button");
 
-class Book {
-    constructor(title, author, pages) {
-      this.title = title;
-      this.author = author;
-      this.pages = pages;
-    }
-  }
+const button = document.getElementById('button');
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
 
 function addBook() {
   const list = document.getElementById('book-list');
-  const row = document.createElement("tr");
+  const row = document.createElement('tr');
 
   row.innerHTML = `
                     <td> <input type="checkbox" onClick="update()" /> </td>
                     <td style="flex-grow: 2"> ${title.value} </td>
                     <td style="flex-grow: 2"> ${author.value} </td>
                     <td style="flex-grow: 2"> ${pages.value} </td>
-                    <td> <span onClick="remove()"> X </span> </td>
+                    <td> <span onClick="deleteBook()"> X </span> </td>
                     `;
 
   list.appendChild(row);
@@ -28,39 +24,38 @@ function addBook() {
   document.getElementById('pages').value = '';
 }
 
-button.addEventListener("click", function (e) {
-  if (title.value === "" || author.value === "" || pages.value === "" ) {
-    validation("Do not leave empty fields", "unsuccess");
-  } else {
-    addBook();
-    validation("Booked Added", "success");
-  }
-});
-
-function remove(event) {
-  this.event.target.parentElement.parentElement.remove();
-}
-
 function validation(message, className) {
-  const div = document.createElement("div");
+  const div = document.createElement('div');
   div.className = className;
   div.appendChild(document.createTextNode(message));
 
-  const titulo = document.querySelector("h1");
+  const titulo = document.querySelector('h1');
 
   titulo.insertBefore(div, null);
 
-  setTimeout(function () {
+  setTimeout(() => {
     document.querySelector(`.${className}`).remove();
   }, 3000);
 }
 
-function update(event) {
+function update() {
   if (this.event.target.checked) {
-    this.event.target.parentElement.parentElement.classList.add("read");
+    this.event.target.parentElement.parentElement.classList.add('read');
   } else {
-    this.event.target.parentElement.parentElement.classList.remove("read");
+    this.event.target.parentElement.parentElement.classList.remove('read');
   }
 }
 
-localStorage.setItem()
+function deleteBook() {
+  this.event.target.parentElement.parentElement.remove();
+}
+
+button.addEventListener('click', () => {
+  if (title.value === '' || author.value === '' || pages.value === '') {
+    validation('Do not leave empty fields', 'unsuccess');
+  } else {
+    addBook();
+    validation('Booked Added', 'success');
+    update();
+  }
+});
